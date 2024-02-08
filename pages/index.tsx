a84b975a3d5848bedad102e6ed8a98a6320670eb
TABASCO <84655644+TABASCOatw@users.noreply.github.com>
2024-02-08 11:43:12 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,51 +1,50 @@
-
 import '@particle-network/connect-react-ui/dist/index.css';
 import { ConnectButton } from '@particle-network/connect-react-ui';
 import styles from "../styles/Home.module.css";
 import { NextPage } from "next";
 import { useRouter } from 'next/router';
-  
+
 const Home: NextPage = () => {
   const router = useRouter();
 
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <div className={styles.header}>
-          <div className={styles.column}>
-            <ConnectButton.Custom>
-              {({ account, chain, openAccountModal, openConnectModal, openChainModal, accountLoading }) => {
-                return (
-                  <div>
-                    <button onClick={openConnectModal} disabled={!!account}>
-                      Open Connect
-                    </button>
-                    <br />
-                    <br />
-                    <button onClick={openAccountModal} disabled={!account}>
-                      Open Account
-                    </button>
-                    <br />
-                    <br />  
-                    <button onClick={openChainModal} disabled={!account}>
-                      Open Switch Network
-                    </button>
-                    <br />
-                    <br /> 
-                    <button onClick={() => router.push(`/landing?address=${account}`)} disabled={!account}>
-                      Explore Wallet
-                    </button>
-                    <div>
-                      <h3>account</h3>
-                      <p>{account}</p>
-                    </div>
-                  </div>
-                );
-              }}
-            </ConnectButton.Custom>
-          </div>
-        </div>
-        <div className={styles.grid}></div>
+        <ConnectButton.Custom>
+          {({ account, openAccountModal, openConnectModal, openChainModal }) => {
+            if (!account) {
+              return (
+                <div className={styles.buttonContainer}>
+                  <button onClick={openConnectModal} className={styles.button}>
+                    <img src="https://i.imgur.com/JTPDyCN.png" alt="Logo" className={styles.buttonLogo} />
+                    <span>Open Connect</span>
+                  </button>
+
+                </div>
+              );
+            }
+            return (
+              <div className={styles.buttonContainer}>
+                <button onClick={openAccountModal} className={`${styles.button} ${styles.greyButton}`} disabled={!account}>
+                  Open Account
+                </button>
+                <button onClick={openChainModal} className={`${styles.button} ${styles.greyButton}`} disabled={!account}>
+                  Open Switch Network
+                </button>
+                <button
+                  onClick={() => router.push(`/landing?address=${account}`)}
+                  className={`${styles.button} ${styles.exploreWalletButton}`}
+                >
+                  Explore Wallet
+                </button>
+                <div className={styles.addressContainer}>
+                  <h3>Address</h3>
+                  <p>{account}</p>
+                </div>
+              </div>
+            );
+          }}
+        </ConnectButton.Custom>
       </div>
     </main>
   );
